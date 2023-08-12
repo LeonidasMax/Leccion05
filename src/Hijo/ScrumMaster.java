83158a84899d66b9e7593615bdf3c88c1a2254b0
Leonidas <ovalladareso@miumg.edu.gt>
2023-08-12 10:42:47 -0600

    Initial commit

--- src/Hijo/ScrumMaster.java
+++ src/Hijo/ScrumMaster.java
@@ -1,2 +1,32 @@
-package Hijo;public class ScrumMaster {
+package Hijo;
+
+import Padre.Empleado;
+
+public class ScrumMaster extends Empleado {
+//    private String departamento;
+
+    public String getDepartamento(){
+        return departamento;
+    };
+
+
+    public void setDepartamento(){};
+    public ScrumMaster(String nombre, double sueldo, String depto) {
+        super(nombre, sueldo);
+        this.departamento = depto;
+    }
+
+        @Override
+                public  String obtenerDetalles(){
+                return "Des Hijo Nombre: "+this.getNombre()+" sueldo"+getSueldo()+"dire: "+this.getDepartamento();
+
+        }
+
+    }
+
 }
+@Override
+    public String tosString(){
+        return "su nombre es+ "+this.getNombre()+ "sueldo: "+this.getSueldo();
+
+        }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main {
+import Hijo.ScrumMaster;
+import Padre.Empleado;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void  imprime(Empleado emple){
+        System.out.println("Objeto.obtenerDetalles()="+emple.obtenerDetalles());
+    }
+    public static void prueba(int dato) {
+        System.out.println("nada");
+
+    }
+
+    public static void  determinarTipo(Empleado emple){
+        if (emple instanceof ScrumMaster) {
+            System.out.println("Esto viene de ScrumMaster");
+        } else if (emple instanceof Empleado){
+            System.out.println("Esto viene de empleado");
+        } else if (emple instanceof Object){
+            System.out.println("Esto es cualquier cosa");
+        }
+
+        }
+
+
+    public static void main(String[] args) {
+         Empleado em = new Empleado("juan", 3500);
+         imprime(em);
+//         System.out.println(em.obtenerDetalles());
+        ScrumMaster sm = new ScrumMaster("juan", 3000, "desarrollo");
+//        System.out.println(sm.obtenerDetalles());
+        System.out.println(em.toString());
+
+
+
+    }
 }
